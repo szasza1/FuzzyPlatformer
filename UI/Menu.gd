91d06185ee93f29392709820f759550624f9fd39
Szasz Attila <szasz@inf.u-szeged.hu>
2023-05-06 15:23:18 +0200

    Little fixes

--- UI/Menu.gd
+++ UI/Menu.gd
@@ -6,10 +6,5 @@ func _ready():
 func _on_start_pressed():
 	get_tree().change_scene_to_file("res://Stages/Stage1.tscn")
 
-
-func _on_options_pressed():
-	pass # Replace with function body.
-
-
 func _on_exit_pressed():
 	get_tree().quit()
